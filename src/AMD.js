febd4549f903c70a57a6429ade7ca9eeccb9a082
Qusai Tabbal <qtabbal@souq.com>
2016-01-14 12:16:18 +0200

    init commit

---
+++ src/AMD.js
@@ -0,0 +1,20 @@
+/**
+ * A module representing a jacket.
+ * @module my/jacket
+ */
+define('my/jacket', function() {
+    /**
+     * @constructor
+     * @alias module:my/jacket
+     */
+    var Jacket = function() {
+        // ...
+    };
+
+    /** Zip up the jacket. */
+    Jacket.prototype.zip = function() {
+        // ...
+    };
+
+    return Jacket;
+});
---
+++ src/book.js
@@ -0,0 +1,12 @@
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ */
+function Book(title, author) {
+
+	/** This is a description of the foo function. */
+	function foo() {
+	}
+}
